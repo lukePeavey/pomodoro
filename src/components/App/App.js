280662dc5a135da2b36f48351ea426a39749913b
Luke Peavey <1williampeavey@gmail.com>
2018-08-18 00:46:45 -0400

    Create react app

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class App extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <span>App Stub</span>
+      </div>
+    )
+  }
+}
