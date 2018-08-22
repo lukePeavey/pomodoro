0b751a55822f4d89b6809176ff0a4cabbf70bc4e
Luke Peavey <1williampeavey@gmail.com>
2018-08-22 13:17:12 -0400

    Build the Timer component

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Button from '@material-ui/core/Button'
+import ResetIcon from '@material-ui/icons/SettingsBackupRestore'
+import IconButton from '@material-ui/core/IconButton'
+import { requestAnimationFrame, cancelAnimationFrame } from '../../utils'
 
 /**
  * Handles the timer logic - updating state while timer is running,
@@ -14,23 +18,93 @@ import PropTypes from 'prop-types'
 export default class Timer extends React.PureComponent {
   state = { requestID: null }
 
+  componentDidMount = () => {
+    // Initialize a new timer on mount
+    this.props.actions.initializeTimer()
+  }
+
   /** Updates the timer state on each RAF callback */
-  updateTimer = (timestamp) => {}
+  updateTimer = (timestamp) => {
+    const { startTime, duration, actions } = this.props
+    const elapsedTime = timestamp - startTime
+
+    // When the timer reaches zero, start the next session
+    if (elapsedTime >= duration) {
+      this.startNextSession()
+    } else {
+      // Update the elapsed time value in state.
+      actions.updateElapsedTime(elapsedTime)
+      this.setState({ requestID: requestAnimationFrame(this.updateTimer) })
+    }
+  }
 
   /** Starts the next work/break session. */
-  startNextSession = () => {}
+  startNextSession = () => {
+    const { type: previousType, actions } = this.props
+    actions.stopTimer()
+    actions.initializeTimer({
+      type: previousType === 'SESSION' ? 'BREAK' : 'SESSION',
+      startTime: window.performance.now(),
+      isRunning: true
+    })
+  }
 
   /** Listen for changes to props.isRunning */
-  componentDidUpdate = (prevProps) => {}
+  componentDidUpdate = (prevProps) => {
+    // When the timer is stopped/started...
+    if (prevProps.isRunning !== this.props.isRunning) {
+      // Start running the updateTimer loop when the timer is started
+      if (this.props.isRunning) {
+        cancelAnimationFrame(this.state.requestID)
+        this.setState({ requestID: requestAnimationFrame(this.updateTimer) })
+      }
+      // Stop the updateTimer loop when the timer is stopped/paused
+      if (!this.props.isRunning) {
+        this.setState(({ requestID }) => ({
+          requestID: cancelAnimationFrame(requestID)
+        }))
+      }
+    }
+  }
 
   /** Event handler for the start/stop button */
-  handleToggleButtonClick = (event) => {}
+  handleToggleButtonClick = (event) => {
+    const { isRunning, actions } = this.props
+    const timestamp = window.performance.now()
+    const toggleTimer = isRunning ? 'stopTimer' : 'startTimer'
+    actions[toggleTimer](timestamp)
+  }
 
   /** Event handler for the reset button */
-  handleResetButtonClick = (event) => {}
+  handleResetButtonClick = (event) => {
+    const { actions } = this.props
+    actions.stopTimer()
+    actions.restoreDefaults()
+    actions.initializeTimer()
+  }
 
   render() {
-    return <div>This is a stub</div>
+    const { remainingTimeString, isRunning, type } = this.props
+    return [
+      <section className="Timer" key="timer">
+        <div className="Timer-content">
+          <h1 className="Timer-label" id="timer-label">
+            {type}
+          </h1>
+          <div id="time-left" className="Timer-displayTime">
+            {remainingTimeString}
+          </div>
+          <div className="controls">
+            <IconButton onClick={this.handleResetButtonClick}>
+              <ResetIcon />
+            </IconButton>
+            <Button id="start_stop" onClick={this.handleToggleButtonClick}>
+              {isRunning ? 'Stop' : 'Start'}
+            </Button>
+          </div>
+        </div>
+      </section>
+    ]
   }
 }
 
---
+++ src/components/Timer/TimerContainer.js
@@ -0,0 +1,27 @@
+import { connect } from 'react-redux'
+import { actions, selectors } from '../../store/'
+import bindActions from '../../utils/bindActions'
+import Timer from './Timer'
+
+const mapStateToProps = (state) => ({
+  duration: selectors.getDuration(state),
+  type: selectors.getTimerType(state),
+  remainingTimeString: selectors.getRemainingTimeString(state),
+  elapsedTime: selectors.getElapsedTime(state),
+  isRunning: selectors.isRunning(state),
+  sessionLength: selectors.getSessionLength(state),
+  startTime: selectors.getStartTime(state)
+})
+
+const mapDispatchToProps = bindActions({
+  updateElapsedTime: actions.updateElapsedTime,
+  initializeTimer: actions.initializeTimer,
+  stopTimer: actions.stopTimer,
+  startTimer: actions.startTimer,
+  restoreDefaults: actions.restoreDefaults
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Timer)
